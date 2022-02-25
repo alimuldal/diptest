bfc4c8e983df3d6fc7c53b740f32081343bc107b
Ralph Urlus <rurlus.dev@gmail.com>
2022-02-25 22:40:50 +0100

    ENH: [CPP] Wrapper functions around dipst and pvalue computation

---
+++ include/diptest/wrapper.hpp
@@ -0,0 +1,82 @@
+/* wrapper.hpp -- header file for wrapper around diptest implementation
+ * Copyright 2022 R. Urlus
+ */
+#ifndef INCLUDE_DIPTEST_WRAPPER_HPP_
+#define INCLUDE_DIPTEST_WRAPPER_HPP_
+#include <pybind11/pybind11.h>
+#include <pybind11/numpy.h>
+#include <pybind11/stl.h>
+
+extern "C" {
+#include <diptest/diptest.h>
+}
+#include <cmath>             // NAN
+#include <stdexcept>         // runtime_error
+#include <algorithm>         // sort
+#include <random>            // mt19937_64, uniform_real_distribution
+#include <numeric>           // accumulate
+#include <memory>            // unique_ptr
+
+
+#if defined(_WIN32) || defined(_WIN64) || defined(WIN32) || defined(__CYGWIN__) || defined(__MINGW32__) || defined(__BORLANDC__)
+#define OS_WIN
+#endif
+
+// handle error C2059: syntax error: ';'  on windows for this Macro
+#if not defined(OS_WIN)
+  #define STRINGIFY(x) #x
+  #define MACRO_STRINGIFY(x) STRINGIFY(x)
+#endif
+
+// Fix for lack of ssize_t on Windows for CPython3.10
+#if defined(_MSC_VER)
+#  pragma warning(push)
+#  pragma warning(disable: 4127) // warning C4127: Conditional expression is constant
+#include <BaseTsd.h>
+typedef SSIZE_T ssize_t;
+#endif
+
+namespace py = pybind11;
+
+namespace diptest {
+
+namespace details {
+void draw_sorted_std_uniform(double* dest, const size_t size, const size_t seed = 0);
+double* std_uniform(const int64_t n_boot, const int64_t n, const int64_t seed);
+double diptest(const double* x_ptr, int N, int allow_zero = 1, int debug = 0);
+}  // namespace details
+
+double diptest(const py::array_t<double>&x, int allow_zero, int debug);
+py::dict diptest_full(const py::array_t<double>&x, int allow_zero, int debug);
+double diptest_pval(
+    const double dipstat,
+    const int64_t n,
+    const int64_t n_boot,
+    int allow_zero,
+    int debug,
+    int seed
+);
+
+double diptest_pval_mt(
+    const double dipstat,
+    const int64_t n,
+    const int64_t n_boot,
+    int allow_zero,
+    int debug,
+    int seed,
+    int n_threads
+
+);
+
+namespace bindings {
+
+void bind_diptest(py::module &m);
+void bind_diptest_full(py::module &m);
+void bind_diptest_pval(py::module &m);
+void bind_diptest_pval_mt(py::module &m);
+
+
+}  // namespace bindings
+}  // namespace diptest
+
+#endif  // INCLUDE_DIPTEST_WRAPPER_HPP_
---
+++ src/wrapper.cpp
@@ -0,0 +1,228 @@
+/* wrapper.cpp -- implementation of wrapper around diptst from diptest.c
+ * Copyright 2022 R. Urlus
+ */
+
+#include <diptest/wrapper.hpp>
+
+namespace py = pybind11;
+
+namespace diptest {
+
+namespace details {
+
+double* std_uniform(const int64_t n_boot, const int64_t n, const int64_t seed) {
+    std::mt19937_64 rng;
+    rng.seed(seed);
+    std::uniform_real_distribution<double> dist(0.0, 1.0);
+    int64_t size = n_boot * n;
+    double* sample = new double[size];
+    for (int64_t i = 0; i < size; i++) {
+        sample[i] = dist(rng);
+    }
+    return sample;
+}
+
+double diptest(const double* x_ptr, int N, int allow_zero, int debug) {
+    double dip = NAN;
+    int ifault = 0;
+    int lo_hi[4] = {0, 0, 0, 0};
+    std::unique_ptr<int[]> gcm(new int[N]);
+    std::unique_ptr<int[]> lcm(new int[N]);
+    std::unique_ptr<int[]> mn(new int[N]);
+    std::unique_ptr<int[]> mj(new int[N]);
+
+    diptst(
+        x_ptr,
+        &N,
+        &dip,
+        &lo_hi[0],
+        &ifault,
+        gcm.get(),
+        lcm.get(),
+        mn.get(),
+        mj.get(),
+        &allow_zero,
+        &debug
+    );
+
+    if (ifault == 1) {
+        throw std::runtime_error("N must be >= 1.");
+    } else if (ifault == 2) {
+        throw std::runtime_error("x must be sorted in ascending error.");
+    }
+    return dip;
+} // diptest
+
+}  // namespace details
+
+double diptest(const py::array_t<double>&x, int allow_zero, int debug) {
+    return details::diptest(x.data(), x.size(), allow_zero, debug);
+} // diptest
+
+py::dict diptest_full(const py::array_t<double>&x, int allow_zero, int debug) {
+    const double* x_ptr =  x.data();
+    int N = x.size();
+    double dip = NAN;
+    int ifault = 0;
+    int lo_hi[4] = {0, 0, 0, 0};
+
+    auto gcm = py::array_t<int>(N);
+    auto lcm = py::array_t<int>(N);
+    auto mn = py::array_t<int>(N);
+    auto mj = py::array_t<int>(N);
+    int* gcm_ptr = gcm.mutable_data();
+    int* lcm_ptr = lcm.mutable_data();
+    int* mn_ptr = mn.mutable_data();
+    int* mj_ptr = mj.mutable_data();
+
+    diptst(
+        x_ptr,
+        &N,
+        &dip,
+        &lo_hi[0],
+        &ifault,
+        gcm_ptr,
+        lcm_ptr,
+        mn_ptr,
+        mj_ptr,
+        &allow_zero,
+        &debug
+    );
+
+    if (ifault == 1) {
+        throw std::runtime_error("N must be >= 1.");
+    } else if (ifault == 2) {
+        throw std::runtime_error("x must be sorted in ascending error.");
+    }
+
+    using namespace pybind11::literals; // to bring in the `_a` literal NOLINT
+    return py::dict(
+        "dip"_a = dip,
+        "lo"_a = lo_hi[0],
+        "hi"_a = lo_hi[1],
+        "xl"_a = x.at(lo_hi[0]),
+        "xh"_a = x.at(lo_hi[1]),
+        "mn"_a = mn,
+        "mj"_a = mj,
+        "_gcm"_a = gcm,
+        "_lcm"_a = lcm,
+        "_lh_2"_a = lo_hi[2],
+        "_lh_3"_a = lo_hi[3]
+    );
+} // diptest_full
+
+double diptest_pval(
+    const double dipstat,
+    const int64_t n,
+    const int64_t n_boot,
+    int allow_zero,
+    int debug,
+    int seed
+) {
+    std::random_device rd;
+    if (seed == 0) {
+        seed = rd();
+    }
+
+    double* sample = details::std_uniform(n_boot, n, seed);
+    // needed as we shift the original ptr
+    double* p_sample = sample;
+    double* sample_end = nullptr;
+
+    std::unique_ptr<int[]> dips(new int[n_boot]);
+    for (int64_t i = 0; i < n_boot; i++) {
+        sample_end = sample + n;
+        std::sort(sample, sample_end);
+        dips[i] = dipstat <= details::diptest(sample, n, allow_zero, debug);
+        sample = sample_end;
+    }
+    double p_val = std::accumulate(dips.get(), dips.get() + n_boot, 0.0) / n_boot;
+
+    delete[] p_sample;
+    return p_val;
+} // diptest_pval
+
+double diptest_pval_mt(
+    const double dipstat,
+    const int64_t n,
+    const int64_t n_boot,
+    int allow_zero,
+    int debug,
+    int seed,
+    int n_threads
+) {
+    std::random_device rd;
+    if (seed == 0) {
+        seed = rd();
+    }
+
+    std::unique_ptr<int[]> dips(new int[n_boot]);
+    double* sample = details::std_uniform(n_boot, n, seed);
+    double* p_sample;
+    double* p_sample_end;
+
+    #pragma omp parallel for private(p_sample, p_sample_end) num_threads(n_threads)
+    for (int64_t i = 0; i < n_boot; i++) {
+        p_sample = sample + (i * n);
+        p_sample_end = p_sample + n;
+        std::sort(p_sample, p_sample_end);
+        dips[i] = dipstat <= details::diptest(p_sample, n, allow_zero, debug);
+    }
+    double p_val = std::accumulate(dips.get(), dips.get() + n_boot, 0.0) / n_boot;
+
+    delete[] sample;
+    return p_val;
+} // diptest_pval_mt
+
+
+namespace bindings {
+
+void bind_diptest(py::module &m) {
+    m.def(
+        "diptest",
+        &diptest::diptest,
+        py::arg("x"),
+        py::arg("allow_zero") = 1,
+        py::arg("debug") = 0
+    );
+}
+
+void bind_diptest_full(py::module &m) {
+    m.def(
+        "diptest_full",
+        &diptest::diptest_full,
+        py::arg("x"),
+        py::arg("allow_zero") = 1,
+        py::arg("debug") = 0
+    );
+}
+
+void bind_diptest_pval(py::module &m) {
+    m.def(
+        "diptest_pval",
+        &diptest::diptest_pval,
+        py::arg("dipstat"),
+        py::arg("n"),
+        py::arg("n_boot") = 10000,
+        py::arg("allow_zero") = 1,
+        py::arg("debug") = 0,
+        py::arg("seed") = 0
+    );
+}
+
+void bind_diptest_pval_mt(py::module &m) {
+    m.def(
+        "diptest_pval_mt",
+        &diptest::diptest_pval_mt,
+        py::arg("dipstat"),
+        py::arg("n"),
+        py::arg("n_boot") = 10000,
+        py::arg("allow_zero") = 1,
+        py::arg("debug") = 0,
+        py::arg("seed") = 0,
+        py::arg("n_threads") = 4
+    );
+}
+
+}  // namespace bindings
+}  // namespace diptest
