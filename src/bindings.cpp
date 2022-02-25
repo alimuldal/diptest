0ea85088e8ced76bb3e086695f9e0bee07de6cf4
Ralph Urlus <rurlus.dev@gmail.com>
2022-02-25 22:42:02 +0100

    BLD: [CPP] Bindings and build instructions

---
+++ src/bindings.cpp
@@ -0,0 +1,26 @@
+/* bindings.cpp -- Python bindings for diptest
+ * Copyright 2022 Ralph Urlus
+ */
+#include <pybind11/pybind11.h>
+#include <diptest/wrapper.hpp>
+
+namespace diptest {
+namespace bindings {
+
+PYBIND11_MODULE(EXTENSION_MODULE_NAME, m) {
+    bind_diptest(m);
+    bind_diptest_full(m);
+    bind_diptest_pval(m);
+    bind_diptest_pval_mt(m);
+
+#if not defined OS_WIN
+  #ifdef DIPTEST_VERSION_INFO
+      m.attr("__version__") = MACRO_STRINGIFY(DIPTEST_VERSION_INFO);
+  #else
+      m.attr("__version__") = "dev";
+  #endif
+#endif
+}
+
+}  // namespace bindings
+}  // namespace diptest
